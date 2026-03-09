c9fe9337684ed6ebf029ae4075e5acc0ebfcca44
Erik Plaza Morales <erikplzm@gmail.com>
2026-03-09 12:33:23 +0100

    Métodos básicos de la interfaz de texto

---
+++ src/jcolonia/daw2025/tablasmvc/VistaGeneral.java
@@ -0,0 +1,49 @@
+package jcolonia.daw2025.tablasmvc;
+
+import java.util.Scanner;
+
+public class VistaGeneral {
+    private Scanner scanner;
+
+    public VistaGeneral() {
+        scanner = new Scanner(System.in);
+    }
+
+    // Mostrar texto normal
+    public void mostrarTexto(String texto) {
+        System.out.println(texto);
+    }
+
+    // Mostrar aviso
+    public static void mostrarAviso(String texto) {
+        System.out.println("⚠ " + texto);
+    }
+
+    // Mostrar título principal
+    public void mostrarTitulo1(String texto) {
+        System.out.println("\n=== " + texto.toUpperCase() + " ===");
+    }
+
+    // Mostrar título secundario
+    public void mostrarTitulo2(String texto) {
+        System.out.println("\n-- " + texto + " --");
+    }
+
+    // Pedir número al usuario
+    public int pedirNumero(String texto) {
+        System.out.print(texto + ": ");
+        return scanner.nextInt();
+    }
+
+    // Pausa del programa
+    public void pausa(String texto) {
+        System.out.println(texto);
+        System.out.println("Pulsa ENTER para continuar...");
+        try {
+            System.in.read();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
